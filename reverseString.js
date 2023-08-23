1aa91649a88fb2e250bbeef4bc4428bb343fe9ee
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-08-23 15:33:08 +0300

    Implement the  functionality

---
+++ reverseString.js
@@ -0,0 +1,11 @@
+const reverseString = (string) => {
+	if (typeof string !== 'string')
+		throw new Error('Input type must be a string');
+
+	if (string.length <= 1)
+		throw new Error('String must have more than one character');
+
+	return string.split('').reverse().join('');
+};
+
+module.exports = reverseString;
